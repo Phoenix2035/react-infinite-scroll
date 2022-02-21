d70572eea904ea887416f9e4997943991322a202
Ali Gh <aligodosi@gmail.com>
2022-02-21 22:39:07 +0330

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react"
+
+
+function App() {
+
+
+    return (
+        <>
+            <input type="text" />
+            <div>Title</div>
+            <div>Title</div>
+            <div>Title</div>
+            <div>Title</div>
+            <div>Loading...</div>
+            <div>Error</div>
+        </>
+    );
+}
+
+export default App;
